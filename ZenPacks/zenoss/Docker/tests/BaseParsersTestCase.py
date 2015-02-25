dec0f873b40422eed012a604a476b49939bd64bf
Volodymyr Sergeyev <vova.sergeyev@gmail.com>
2015-02-25 17:55:19 +0200

    ZEN-16843: fixes due to comments

--- ZenPacks/zenoss/Docker/tests/BaseParsersTestCase.py
+++ ZenPacks/zenoss/Docker/tests/BaseParsersTestCase.py
@@ -1,3 +1,13 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2015, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+from pprint import pformat
 
 from Products.ZenTestCase.BaseTestCase import BaseTestCase
 from Products.ZenRRD.CommandParser import ParsedResults
