f91f4609b8f5406653beb3b5aedbc26bec432811
Oleksandr Dubrovyk <odubrovyk@zenoss.com>
2022-02-10 15:22:36 +0000

    Add all necessary files for a new component
    Fixes ZPS-7870.

---
+++ ZenPacks/zenoss/Docker/modeler/plugins/PodmanCollector.py
@@ -0,0 +1,90 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2016, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""Collects information about Docker Containers."""
+
+from Products.DataCollector.plugins.CollectorPlugin import CommandPlugin
+from Products.DataCollector.plugins.DataMaps import ObjectMap
+
+from ZenPacks.zenoss.Docker import parsing
+from ZenPacks.zenoss.Docker.utils import command_from_commands, results_from_result
+
+
+class PodmanCollector(CommandPlugin):
+
+    relname = "podman_containers"
+    modname = "ZenPacks.zenoss.Docker.PodmanContainer"
+
+    command = command_from_commands(
+        "podman -v",
+        "sudo podman ps -a --no-trunc",
+        "cat /proc/self/mountinfo",
+        )
+
+    def process(self, device, results, log):
+        log.info('Collecting podman containers for device %s' % device.id)
+        # Change results into a list of of results. One element per command.
+        log.debug('Podman collector full command: %s' % results)
+        # Split commands into list of commands
+        results = results_from_result(results)
+        log.debug('Podman collector list commands: %s' % results)
+        maps = []
+
+        # Map device "docker_version" property.
+        if results[0].startswith("podman version"):
+            log.info("%s: %s", device.id, results[0])
+            maps.append(ObjectMap({'podman_version': results[0]}))
+        else:
+            log.info("%s: no podman version", device.id)
+            maps.append(ObjectMap({"podman_version": None}))
+
+        try:
+            rows = parsing.rows_from_output(
+                results[1],
+                expected_columns=[
+                    "CONTAINER ID",
+                    "IMAGE",
+                    "COMMAND",
+                    "CREATED",
+                    "PORTS",
+                    "NAMES",
+                    ])
+
+        except parsing.MissingColumnsError:
+            log.info("%s: unexpected podman ps output", device.id)
+            return maps
+
+        rm = self.relMap()
+        maps.append(rm)
+
+        if not rows:
+            log.info("%s: no podman containers found", device.id)
+            return maps
+
+        try:
+            cgroup_path = parsing.cgroup_path_from_output(results[2])
+        except parsing.CgroupPathNotFound:
+            log.info("%s: no cgroup path found. The default value '/sys/fs/cgroup' is set", device.id)
+            cgroup_path = "/sys/fs/cgroup"
+
+        for row in rows:
+            rm.append(
+                self.objectMap({
+                    "id": row["CONTAINER ID"],
+                    "title": row["NAMES"],
+                    "image": row["IMAGE"],
+                    "command": row["COMMAND"],
+                    "created": row["CREATED"],
+                    "ports": row["PORTS"],
+                    "cgroup_path": cgroup_path,
+                    }))
+
+        log.info("%s: found %s Podman containers", device.id, len(rm.maps))
+        log.info("Podman RM maps: %s", rm.maps)
+        return maps
---
+++ ZenPacks/zenoss/Docker/utils.py
@@ -0,0 +1,28 @@
+##############################################################################
+#
+# Copyright (C) Zenoss, Inc. 2022, all rights reserved.
+#
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+
+"""Additional function for pocker commands output parsing."""
+
+
+def command_from_commands(*commands):
+    """Return a single command given an iterable of commands.
+
+    The results of each command will be separated by __SPLIT__, and the
+    results will contain only the commands' stdout.
+
+    """
+    return " ; echo __SPLIT__ ; ".join("{} 2>/dev/null".format(c) for c in commands)
+
+
+def results_from_result(result):
+    """Return results split into a list of per-command output."""
+    try:
+        return [r.strip() for r in result.split("__SPLIT__")]
+    except Exception:
+        return []
